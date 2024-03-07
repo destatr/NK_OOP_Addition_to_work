76e02311a8d5571425ba2cd57a57dfe50565ac55
_ <160770918+destatr@users.noreply.github.com>
2024-03-07 18:09:06 +0100

    Add files via upload

---
+++ Discussion.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace NK_OOP_Lab3
+{
+    /// <summary>
+    /// Represents a discussion in the project.
+    /// </summary>
+    public class Discussion
+    {
+        // Fields
+        private List<Message> messages = new List<Message>(); // List of messages in the discussion
+
+        // Properties
+        /// <summary>
+        /// Gets or sets the task associated with the discussion.
+        /// </summary>
+        public Task Task
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        // Methods
+        /// <summary>
+        /// Writes a new message to the discussion.
+        /// </summary>
+        /// <param name="message">The content of the message to write.</param>
+        public void WriteMessage(string message)
+        {
+            messages.Add(new Message { Content = message });
+        }
+
+        /// <summary>
+        /// Sends a message to the discussion.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        public void SendMessage(Message message)
+        {
+            // Implementation goes here
+        }
+
+        /// <summary>
+        /// Changes an existing message in the discussion.
+        /// </summary>
+        /// <param name="message">The message to change.</param>
+        public void ChangeMessage(Message message)
+        {
+            // Implementation goes here
+        }
+
+        /// <summary>
+        /// Deletes a message from the discussion.
+        /// </summary>
+        /// <param name="message">The message to delete.</param>
+        public void DeleteMessage(Message message)
+        {
+            // Implementation goes here
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NK_OOP_Lab3
+{
+    /// <summary>
+    /// Represents a member of the project.
+    /// </summary>
+    public class Member : User
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Member"/> class with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the member.</param>
+        public Member(string name) : base(name) { }
+    }
+}
